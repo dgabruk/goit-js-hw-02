6d39bb3174b1032e36081cc2aa3a3df00a68334e
dgabruk <deathcore1315@gmail.com>
2024-08-31 20:43:35 +0300

    done

--- script/task-4.js
+++ script/task-4.js
@@ -1,18 +1,19 @@
 'use strict'
 
 function getShippingCost(country) {
+    let 
     switch (country) {
         case 'China':
-            return 'Shipping to China will cost 100 credits';
+            return `Shipping to ${country} will cost 100 credits`;
             break;
         case 'Chile':
-            return 'Shipping to Chile will cost 250 credits';
+            return `Shipping to ${country} will cost 250 credits`;
             break;
         case 'Australia':
-            return 'Shipping to Australia will cost 170 credits';
+            return `Shipping to ${country} will cost 170 credits`;
             break;
         case 'Jamaica':
-            return 'Shipping to Jamaica will cost 120 credits';
+            return `Shipping to ${country} will cost 120 credits`;
             break;
         default:
             return "Sorry, there is no delivery to your country";
